55b1cc29b1ed1fad49e1e0d4173a7ecf88e64ff9
henrytrinh <trinhdangchinh@gmail.com>
2022-11-29 11:15:10 +0700

    them ham menu

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -22,7 +22,7 @@ export default function MainLayout({ children }: LayoutProps) {
         </div>
 
         {/* <!-- Nav PC --> */}
-        <div className="nav__pc relative flex invisible lg:visible mx-[80px] w-[calc(100%-500px)] text-w">
+        <div className="nav__pc relative flex items-center invisible lg:visible mx-[80px] w-[calc(100%-500px)] text-w">
           <ul className="nav__list flex ">
             {menuList.map((item) => (
               <li key={item} className="nav__link mx-[1vw]">
@@ -32,42 +32,74 @@ export default function MainLayout({ children }: LayoutProps) {
               </li>
             ))}
           </ul>
-          <div className="header_laithu-btn right-0 absolute bg-prim px-6 py-2 content-center top-[50%] translate-y-[-50%] h-min rounded-[10px] onClick=showHide('header-form_shh')">
+          <div className="header_laithu-btn right-0 absolute bg-prim px-6 py-2  h-min rounded-[10px] onClick=showHide('header-form_shh')">
             ĐĂNG KÝ LÁI THỬ
           </div>
         </div>
 
         {/* <!-- End Nav PC -->
                 <!-- Nav mob --> */}
-        {/* <div className="nav_mob l-0 m-uh c-uh invisible">
-            <div
-              id="nav__mb"
-              className="nav__mb nav__mb-showhide l-0 m-uh c-12"
+        <div className="nav_mob visible lg:invisible w-[80px] h-[100%] flex items-center">
+          <div className="w-[30px] h-[30px] text-w ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="bars"
+              className="svg-inline--fa fa-bars fa-w-14"
+              role="img"
+              viewBox="0 0 448 512"
             >
-              <ul className="nav__mb-list">
-                <li className="col c-9 box-m1 ">
-                  <Link href="" className="nav__mb-link typo-1 text-cw">
-                    Danh muc
-                  </Link>
-                </li>
-                <li className="col c-9 box-m1 ">
-                  <Link href="" className="nav__mb-link typo-1 text-cw">
-                    Danh muc
-                  </Link>
-                </li>
-                <li className="col c-9 box-m1 ">
-                  <Link href="" className="nav__mb-link typo-1 text-cw">
-                    Danh muc
-                  </Link> */}
-        {/* </li>
-                <li className="col c-9 box-m1 ">
-                  <Link href="" className="nav__mb-link typo-1 text-cw">
-                    Danh muc
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div> */}
+              <path
+                fill="currentColor"
+                d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
+              />
+            </svg>
+          </div>
+
+          <div className="w-[30px] h-[30px] text-w invisible ">
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="times"
+              className="svg-inline--fa fa-times fa-w-11"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 352 352"
+            >
+              <path
+                id="svg_1"
+                d="m207.6,161l107.72,-107.72c6.23,-6.23 6.23,-16.34 0,-22.58l-25.03,-25.03c-6.23,-6.23 -16.34,-6.23 -22.58,0l-107.71,107.73l-107.72,-107.72c-6.23,-6.23 -16.34,-6.23 -22.58,0l-25.02,25.02c-6.23,6.23 -6.23,16.34 0,22.58l107.72,107.72l-107.72,107.72c-6.23,6.23 -6.23,16.34 0,22.58l25.03,25.03c6.23,6.23 16.34,6.23 22.58,0l107.71,-107.73l107.72,107.72c6.23,6.23 16.34,6.23 22.58,0l25.03,-25.03c6.23,-6.23 6.23,-16.34 0,-22.58l-107.73,-107.71z"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </div>
+
+          <div className="nav_mob l-0 m-uh c-uh ">
+            {/* <div id="nav__mb" className="nav__mb nav__mb-showhide l-0 m-uh c-12">
+                        <ul className="nav__mb-list">
+                            <li className="col c-9 box-m1 ">
+                                <a href="" className="nav__mb-link typo-1 text-cw">Danh muc
+                                </a>
+                            </li>
+                            <li className="col c-9 box-m1 ">
+                                <a href="" className="nav__mb-link typo-1 text-cw">Danh muc
+                                </a>
+                            </li>
+                            <li className="col c-9 box-m1 ">
+                                <a href="" className="nav__mb-link typo-1 text-cw">Danh muc
+                                </a>
+                            </li>
+                            <li className="col c-9 box-m1 ">
+                                <a href="" className="nav__mb-link typo-1 text-cw">Danh muc
+                                </a>
+                            </li>
+                        </ul>
+                    </div> */}
+          </div>
+        </div>
         {/* <!-- End nav mobile --> */}
       </div>
       <div className="sk__overlay nav__mb-showhide header-form_shh"></div>
